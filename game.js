e67fdc7f3b3ffdf2d7a8dcf43d71a815a71f4fa4
mtc-20 <mamenthomas.c@gmail.com>
2020-08-09 16:52:52 +0200

    rel paths correction

--- game.js
+++ game.js
@@ -112,7 +112,7 @@ getNewQuestion = () => {
     // Game over
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
-        return window.location.assign('/end.html');
+        return window.location.assign('./end.html');
     }
     questionCounter ++;
     // questionCounterText.innerText = questionCounter +"/"+ MAX_QUESTIONS;
